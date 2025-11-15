7347f656c71070127c41c48e2264ac043538bf1a
hasan <uthasan2024@gmail.com>
2025-11-15 10:33:42 +0700

    upload ulang

---
+++ mata-kuliah/semester1/script.js
@@ -0,0 +1,9 @@
+// Animasi scroll navbar
+window.addEventListener("scroll", () => {
+  const navbar = document.querySelector(".navbar");
+  if (window.scrollY > 50) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+});
